fe5e19ea7e5af13c8f6a22b4939da44f89e57b55
zhaoll <82825287@qq.com>
2021-06-13 20:21:04 +0800

    '测试'

---
+++ day09/src/com/doit/demo/demo.java
@@ -0,0 +1,15 @@
+package com.doit.demo;
+
+/**
+ * @ClassName: demo
+ * @Author: zll
+ * @CreateTime: 2021/6/13 15:51
+ * @Desc: java 程序
+ * @Version: 1.0
+ */
+public class demo {
+    public static void main(String[] args) {
+        int[] a= new  int[3];
+        System.out.println(a[3]);
+    }
+}
