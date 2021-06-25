87b9ec3ddb23915ab80e49ad442218ea1c8d3fa9
zhaoll <82825287@qq.com>
2021-06-25 14:55:11 +0800

    标记下划线

--- day16/src/com/doit/DemoMap/HashMapDemo.java
+++ day16/src/com/doit/DemoMap/HashMapDemo.java
@@ -1,6 +1,7 @@
 package com.doit.DemoMap;
 
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Set;
 
@@ -11,6 +12,10 @@ import java.util.Set;
  * @Desc: java 程序
  * @Version: 1.0
  */
+//HashMap 默认的初始容量为 16 ;变成树型结构的临界值为 8; 恢复链式结构的临界值为 6
+//当哈希表的大小超过这个阈值static final int MIN_TREEIFY_CAPACITY = 64;才会把链式结构转化成树型结构，否则仅采取扩容来尝试减少冲突
+//按照原来的拉链法来解决冲突，如果一个桶上的冲突很严重的话，是会导致哈希表的效率降低至 O（n），而通过红黑树的方式，可以把效率改进至 O（logn）。
+// 相比链式结构的节点，树型结构的节点会占用比较多的空间，所以这是一种以空间换时间的改进方式。
 public class HashMapDemo {
     public static void main(String[] args) {
         HashMap<Person,String> map = new HashMap<Person, String>();
@@ -18,6 +23,7 @@ public class HashMapDemo {
         map.put(new Person("bbb",28),"撒谎");
         map.put(new Person("ccc",18),"上海");
         map.put(new Person("ccc",18),"世界");
+        //map.put(null,null);//HashMap允许null值和null键
         System.out.println(map);
 
         Set<Person> set = map.keySet();
@@ -29,5 +35,6 @@ public class HashMapDemo {
         for (Map.Entry<Person, String> en : set2) {
             System.out.println(en.getKey().getName()+en.getKey().getAge()+en.getValue());
         }
+
     }
 }
